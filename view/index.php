ddb798a59ab283b034180677a810aa460ceed270
vitorbrangioni <vitorbrangioni@vitorbrangioni-p7-1060br>
2017-03-07 11:14:27 -0300

    Prototipo tela.

--- view/index.php
+++ view/index.php
@@ -1,1 +1,43 @@
 <?php
+include_once 'includes/head.html';
+?>
+
+<body class="container container-full">
+	<div class="row container container-full">
+
+		<div class="btn-group-vertical">
+			<button type="button" class="btn btn-link"><strong>p:</strong> pedro esta falando
+				a verdade</button>
+			<button type="button" class="btn btn-link"><strong>c:</strong> há um cachorro no
+				banheiro</button>
+			<button type="button" class="btn btn-link"><strong>a	:</strong> O arroz é integral</button>
+
+		</div>
+		<div class="container pull-right">
+			<div class="row">
+			
+				<div class="btn-group-justified">
+					<a href="#" class="btn btn-primary">e (∧)</a> <a href="#"
+						class="btn btn-primary">ou (v)</a> <a href="#"
+						class="btn btn-primary">ou..ou (⊻)</a> <a href="#"
+						class="btn btn-primary">implicação (→)</a> <a href="#"
+						class="btn btn-primary">negação (~)</a> <a href="#"
+						class="btn btn-primary">equivalência (↔)</a>
+				</div>
+			</div>
+			<div class="panel panel-primary row">
+				<div class="panel-body form-group">
+				
+					<div class="row">
+						<div class="col-md-1 col-md-offset-5">
+							<input type="text" name="" class="form-control" maxlength="1"
+								disabled>
+						</div>
+					</div>
+				</div>
+			</div>
+			<input type="submit" name="submit" value="gerar tabela verdade"
+				class="btn btn-primary">
+		</div>
+	</div>
+</body>
