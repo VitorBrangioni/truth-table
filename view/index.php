2649209e5a8fc690fb59597b13061773350c523f
vitor.brangioni <vitor.brangioni@SQEST125.squadra.com.br>
2017-03-08 20:38:58 -0300

    Inicio de funcionalidade dos botões.

--- view/index.php
+++ view/index.php
@@ -11,17 +11,78 @@
 	<script type="text/javascript">
 		angular.module("tabelaVerdade", []);
 		angular.module("tabelaVerdade").controller('truthTableController', function ($scope){
-
+			$scope.fullExpression = "";
 			$scope.expressions = [
-				{char: "l", phrase: "há um leão no supermercado"},
+				{char: "i", phrase: "há um leão no supermercado"},
 				{char: "p", phrase: "pedro esta mentindo"}
 			]
 
 			$scope.insertExpression = function (expression) {
 				$scope.expressions.push(angular.copy(expression));
 				delete $scope.expression;
-
 			}
+
+			$scope.getLastChar = function (string) {
+				var posUltimaLetra = string.length -1;
+				return string.charAt(posUltimaLetra);
+			}
+
+
+			$scope.lastCharIsLetter = function (string) {
+				var lastChar = $scope.getLastChar(string);
+				var isChar = false;
+
+				$scope.expressions.forEach(function(entry) {
+    				console.log(entry);
+					if (lastChar === entry.char) {
+						isChar = true;
+					}
+				});
+				return isChar;
+			}
+			$scope.insertBtnValue = function (btnValue) {
+				switch (btnValue) {
+					case "e":
+					var isLetter = $scope.lastCharIsLetter($scope.fullExpression);
+					var lastChar = $scope.getLastChar($scope.fullExpression);
+					
+					console.log($scope.lastCharIsLetter($scope.fullExpression));
+						if(isLetter === true) {
+							$scope.fullExpression += "∧";
+						} else {
+							alert("nao e possivel inserir");
+						}
+						
+					break;
+					case "ou":
+						$scope.fullExpression += "v";
+					break;
+					case "ouou":
+						$scope.fullExpression += "⊻";
+					break;
+					case "imp":
+						$scope.fullExpression += "→";
+					break;
+					case "neg":
+						$scope.fullExpression += "~";
+					break;
+					case "equi":
+						$scope.fullExpression += "↔";
+					break;
+					
+					case "(":
+						$scope.fullExpression += "(";
+					break;
+					case ")":
+						$scope.fullExpression += ")";
+					break;
+					default: 
+						$scope.fullExpression += btnValue;
+					
+				}
+				//console.log(btnValue);
+			}
+
 		});
 
 
@@ -50,9 +111,11 @@
 			<button ng-click="insertExpression(expression)" ng-disabled="!expression.phrase || !expression.char" type="button" class="btn btn-primary"> insert expression </button>
 			
 
-			<button  ng-repeat="teste in expressions" type="button" class="btn btn-link">
-				<strong>{{teste.char}}:</strong> {{teste.phrase}}
-			</button>
+			<div ng-repeat="teste in expressions" >
+				<button ng-click="insertBtnValue(teste.char)" type="button" class="btn btn-link">
+					<strong> {{teste.char}}:</strong> {{teste.phrase}}
+				</button>
+			</div>
 
 
 		</div>
@@ -60,16 +123,14 @@
 			<div class="row">
 
 				<div class="btn-group-justified">
-					<a href="#" class="btn btn-primary">e (∧)</a> <a href="#"
-						class="btn btn-primary">ou (v)</a> <a href="#"
-						class="btn btn-primary">ou..ou (⊻)</a> <a href="#"
-						class="btn btn-primary">implicação (→)</a> <a href="#"
-						class="btn btn-primary">negação (~)</a> <a href="#"
-						class="btn btn-primary">equivalência (↔)</a>
-						<a href="#"
-						class="btn btn-primary">(</a>
-						<a href="#"
-						class="btn btn-primary">)</a>
+					<a ng-click="insertBtnValue('e')" href="#" class="btn btn-primary">e (∧)</a>
+					<a ng-click="insertBtnValue('ou')" href="#"	class="btn btn-primary">ou (v)</a>
+					<a href="#" ng-click="insertBtnValue('ouou')" class="btn btn-primary">ou..ou (⊻)</a>
+					<a href="#" ng-click="insertBtnValue('imp')" class="btn btn-primary">implicação (→)</a>
+					<a href="#" ng-click="insertBtnValue('neg')" class="btn btn-primary">negação (~)</a>
+					<a href="#"	ng-click="insertBtnValue('equi')" class="btn btn-primary">equivalência (↔)</a>
+						<a href="#" ng-click="insertBtnValue('(')" class="btn btn-primary">(</a>
+						<a href="#" ng-click="insertBtnValue(')')" class="btn btn-primary">)</a>
 				</div>
 			</div>
 			<div class="panel panel-primary row">
@@ -100,13 +161,14 @@
 							<!-- <select class="form-control" ng-model="expression.charAndPhrase" ng-options="expression.char for expression in expressions">
 								<option value="">Select a char</option>
 							</select> -->
-							<input class="form-control" type="text" name="tste" value="a^b->c" disabled>
+							<input class="form-control" type="text" name="tste" value="{{fullExpression}}" disabled>
+							
 						</div> 
 					</div>
 				</div>
 				{{expression}}
 			</div>
-			<input type="submit" name="submit" value="gerar tabela verdade"
+			<input ng-click="ultimoCaracter()" type="submit" name="submit" value="gerar tabela verdade"
 				class="btn btn-primary">
 		</div>
 	</div>
