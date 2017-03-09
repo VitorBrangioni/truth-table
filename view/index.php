c2e191b6788edc832bc9291fafd963e600cb0e37
vitorbrangioni <vitorbrangioni@vitorbrangioni-p7-1060br>
2017-03-09 01:03:58 -0300

    Regras ao inserir chars.

--- view/index.php
+++ view/index.php
@@ -12,12 +12,46 @@
 		angular.module("tabelaVerdade", []);
 		angular.module("tabelaVerdade").controller('truthTableController', function ($scope){
 			$scope.fullExpression = "";
+
 			$scope.expressions = [
-				{char: "i", phrase: "há um leão no supermercado"},
-				{char: "p", phrase: "pedro esta mentindo"}
+				{SimpleProposition: "i", phrase: "há um leão no supermercado"},
+				{SimpleProposition: "p", phrase: "pedro esta mentindo"}
 			]
 
-			$scope.insertExpression = function (expression) {
+			$scope.parentheseEnum = {
+				CLOSED : 0,
+				OPENED : 1
+			}
+
+			$scope.verifyParentheses = function () {
+				$scope.fullExpression;
+
+				var expression = $scope.fullExpression;
+				var countParentheseOpened = 0;
+				var countParentheseClosed = 0;
+
+				for (var i = 0; i < expression.length; i++) {
+					var char = expression.charAt(i);
+
+					if (char === "(") {
+						countParentheseOpened++;
+					} else if (char === ")") {
+						countParentheseClosed++;
+					}
+				}
+				return subParentheses = countParentheseOpened - countParentheseClosed;
+			}
+
+			$scope.insertParenthese = function (parenthese) {
+				var verifiedParentheses = $scope.verifyParentheses();
+				if (verifiedParentheses > 0 && parenthese === $scope.parentheseEnum.CLOSED) {
+					$scope.fullExpression += ")";
+				} else if (verifiedParentheses === 0 && parenthese === $scope.parentheseEnum.OPENED) {
+					$scope.fullExpression += "(";
+				}
+			}
+
+			$scope.insertPhrase = function (expression) {
 				$scope.expressions.push(angular.copy(expression));
 				delete $scope.expression;
 			}
@@ -27,61 +61,39 @@
 				return string.charAt(posUltimaLetra);
 			}
 
+			$scope.insertLogicOperations = function (simbol) {
+				var isLetter = $scope.lastCharIsLetter($scope.fullExpression);
+				var lastChar = $scope.getLastChar($scope.fullExpression);
+					
+				if(isLetter === true) {
+					$scope.fullExpression += simbol;
+				}
+			}
+
+			$scope.insertProposition = function (letter) {
+				var isLetter = $scope.lastCharIsLetter($scope.fullExpression);
+				var lastChar = $scope.getLastChar($scope.fullExpression);
+					
+				if(isLetter === false) {
+					$scope.fullExpression += letter;
+				}
+
+			}
+
 
 			$scope.lastCharIsLetter = function (string) {
 				var lastChar = $scope.getLastChar(string);
 				var isChar = false;
 
-				$scope.expressions.forEach(function(entry) {
+				$scope.expressions.forEach(function (entry) {
     				console.log(entry);
-					if (lastChar === entry.char) {
+					if (lastChar === entry.SimpleProposition) {
 						isChar = true;
 					}
 				});
 				return isChar;
 			}
-			$scope.insertBtnValue = function (btnValue) {
-				switch (btnValue) {
-					case "e":
-					var isLetter = $scope.lastCharIsLetter($scope.fullExpression);
-					var lastChar = $scope.getLastChar($scope.fullExpression);
-					
-					console.log($scope.lastCharIsLetter($scope.fullExpression));
-						if(isLetter === true) {
-							$scope.fullExpression += "∧";
-						} else {
-							alert("nao e possivel inserir");
-						}
-						
-					break;
-					case "ou":
-						$scope.fullExpression += "v";
-					break;
-					case "ouou":
-						$scope.fullExpression += "⊻";
-					break;
-					case "imp":
-						$scope.fullExpression += "→";
-					break;
-					case "neg":
-						$scope.fullExpression += "~";
-					break;
-					case "equi":
-						$scope.fullExpression += "↔";
-					break;
-					
-					case "(":
-						$scope.fullExpression += "(";
-					break;
-					case ")":
-						$scope.fullExpression += ")";
-					break;
-					default: 
-						$scope.fullExpression += btnValue;
-					
-				}
-				//console.log(btnValue);
-			}
+
 
 		});
 
@@ -106,14 +118,14 @@
 	<div class="row container container-full">
 
 		<div class="btn-group-vertical">
-			<input ng-model="expression.char" class="form-control" type="text" name="char" placeholder="char" maxlength="1">
+			<input ng-model="expression.SimpleProposition" class="form-control" type="text" name="SimpleProposition" placeholder="proposition" maxlength="1">
 			<input ng-model="expression.phrase" class="form-control" type="text" name="phrase" placeholder="phrase">
-			<button ng-click="insertExpression(expression)" ng-disabled="!expression.phrase || !expression.char" type="button" class="btn btn-primary"> insert expression </button>
+			<button ng-click="insertPhrase(expression)" ng-disabled="!expression.phrase || !expression.SimpleProposition" type="button" class="btn btn-primary"> insert expression </button>
 			
 
-			<div ng-repeat="teste in expressions" >
-				<button ng-click="insertBtnValue(teste.char)" type="button" class="btn btn-link">
-					<strong> {{teste.char}}:</strong> {{teste.phrase}}
+			<div ng-repeat="expression in expressions" >
+				<button ng-click="insertProposition(expression.SimpleProposition)" type="button" class="btn btn-link">
+					<strong> {{expression.SimpleProposition}}:</strong> {{expression.phrase}}
 				</button>
 			</div>
 
@@ -123,14 +135,14 @@
 			<div class="row">
 
 				<div class="btn-group-justified">
-					<a ng-click="insertBtnValue('e')" href="#" class="btn btn-primary">e (∧)</a>
-					<a ng-click="insertBtnValue('ou')" href="#"	class="btn btn-primary">ou (v)</a>
-					<a href="#" ng-click="insertBtnValue('ouou')" class="btn btn-primary">ou..ou (⊻)</a>
-					<a href="#" ng-click="insertBtnValue('imp')" class="btn btn-primary">implicação (→)</a>
-					<a href="#" ng-click="insertBtnValue('neg')" class="btn btn-primary">negação (~)</a>
-					<a href="#"	ng-click="insertBtnValue('equi')" class="btn btn-primary">equivalência (↔)</a>
-						<a href="#" ng-click="insertBtnValue('(')" class="btn btn-primary">(</a>
-						<a href="#" ng-click="insertBtnValue(')')" class="btn btn-primary">)</a>
+					<a ng-click="insertLogicOperations('∧')" href="#" class="btn btn-primary">e (∧)</a>
+					<a ng-click="insertLogicOperations('v')" href="#"	class="btn btn-primary">ou (v)</a>
+					<a href="#" ng-click="insertLogicOperations('⊻')" class="btn btn-primary">ou..ou (⊻)</a>
+					<a href="#" ng-click="insertLogicOperations('→')" class="btn btn-primary">implicação (→)</a>		
+					<a href="#"	ng-click="insertLogicOperations('↔')" class="btn btn-primary">equivalência (↔)</a>
+					<a href="#" ng-click="insertLogicOperations('~')" class="btn btn-primary">negação (~)</a>
+					<a href="#" ng-click="insertParenthese(parentheseEnum.OPENED)" class="btn btn-primary">(</a>
+					<a href="#" ng-click="insertParenthese(parentheseEnum.CLOSED)" class="btn btn-primary">)</a>
 				</div>
 			</div>
 			<div class="panel panel-primary row">
@@ -156,11 +168,6 @@
 
 					
 						<div class="col-md-12">
-							<!-- <input type="text" name="" class="form-control" maxlength="1"
-								disabled> -->
-							<!-- <select class="form-control" ng-model="expression.charAndPhrase" ng-options="expression.char for expression in expressions">
-								<option value="">Select a char</option>
-							</select> -->
 							<input class="form-control" type="text" name="tste" value="{{fullExpression}}" disabled>
 							
 						</div> 
