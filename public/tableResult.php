3e7ae8d5bbd6ff61cbfcf3813cee1048480388ed
vitorbrangioni <vitorbrangioni@vitorbrangioni-HP-G42>
2017-03-25 19:02:05 -0300

    Convertido conectivos das proposições para linguagem de MD

--- public/tableResult.php
+++ public/tableResult.php
@@ -1,79 +1,62 @@
-<html ng-module="tableResult">
+<html>
 	<head>
 	<script type="text/javascript" src="../view/angular/angular.js"></script>
 	<script>
 
-	 angular.module("tableResult", []);
-	angular.module("tableResult").controller('convertColumnValue', function ($scope){
-
-		
-
-		$scope.replaceConn = function (proposition) {
-			proposition.replace("^", "∧");
-			proposition.replace(">", "→"); 
-			alert(proposition);
-		} 
-		
-		
-	});
-	var a = <?php echo json_encode("hahah"); ?>;
 
 	var propositions = [];
+	var propositionsConverted = [];
 
 	function replaceConn(proposition) {
-		proposition.replace("^", "∧");
+		if (proposition.indexOf("^") != -1) {
+			proposition = proposition.replace("^", "∧");
+		}
+		if (proposition.indexOf(">") != -1) {
+			proposition = proposition.replace(">", "→");
+		}
+		if (proposition.indexOf("=") != -1) {
+			proposition = proposition.replace("=", "↔");
+		}
+		if (proposition.indexOf("|") != -1) {
+			proposition = proposition.replace("|", "⊻");
+		}
+		return proposition;
 	}
 	
 	function replaceAll() {
 		for (var i = 0; i < propositions.length; i++) {
-			propositions[i] = replaceConn(propositions[i]);
+			propositionsConverted[i] = replaceConn(propositions[i]);
 		}
 	}
-
-	
-
-	
 	</script>
 	</head>
 	
-	<body ng-controller="convertColumnValue">
+	<body>
 		<table class="table table-hover">
 		 	<?php
-
-		 	
 		 	echo '<thead>';
 		 	echo '<tr>';
 		 	for ($column = 0; $column < count($columns[0]); $column++) {
-		 		// echo '<input type="hidden" value="'.$arroz .'">';
-				//echo '<th><script> test += '. $columns[0][$column]->getPropositionValue() .';alert(test);</script></th>';
-				echo '<th><script> propositions['.json_encode($column).'] = '. json_encode($columns[0][$column]->getPropositionValue()).'</script></th>';
+				echo '<script> propositions['.json_encode($column).'] = '. json_encode($columns[0][$column]->getPropositionValue()).'</script>';
 		 	}
-		 	
-		 	/* echo('<script>
-		 			for (var i; i < propositions.length; i++) {
-		 			
-		 			alert(propositions[i]);
-		 			
+		 	echo '<script>replaceAll();</script>';
+
+		 	 echo('<script>
+		 			for (var i = 0; i < propositionsConverted.length; i++) {
+			 			document.write("<th>"+ propositionsConverted[i] +"</th>");
 		 			}
-		 			
-		 			</script>'); */
-		 	
-		 	echo '<script>replaceAll(); alert(propositions[0]);</script>';
-		 	echo '<script>alert(propositions.length)</script>';
-		 	
+		 		</script>');
 		 	echo '</tr>';
 		 	echo '</thead>';
 		 	
 		 	echo '<tbody>';
-		 	
-		 	/* for ($row = 1; $row <= $truthTable->getNLines(); $row++) {
+		 	 for ($row = 1; $row <= $truthTable->getNLines(); $row++) {
 		 		echo '<tr>';
 		 		for ($column = 0; $column < count($columns[0]); $column++) {
 		 			echo '<th>'. $columns[$row][$column] . '</th>';
 		 		}
 		 		echo '</tr>';
-		 	} */
-		 	
+		 	} 
 		 	echo '</tbody>';
 		 	?>
 		</table>
